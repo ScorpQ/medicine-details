30deee092ca8261b1ffacb738f5866c26f00dba1
ScorpN <sf_commander@outlook.com>
2023-11-19 20:42:39 +0300

    The static post process has been completed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import data from './Services'
+import Medicine from './Services'
 import './App.css'
 import { useEffect } from 'react'
 
 function App() {
   useEffect(() => {
-    data.getUser()
+    Medicine.getUser()
   })
 
   return (
@@ -17,7 +17,9 @@ function App() {
         <input placeholder='Enter your password here' />
       </div>
       <div>
-        <input type='button' value={'Log in'} />
+        <input type='button' value={'Log in'} onClick={() => {
+          Medicine.login('30328905022','1234')
+        }} />
       </div>
     </>
   )
