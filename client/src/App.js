6067dbe0fa39424cc8bdab47d335e230faba4752
ScorpN <sf_commander@outlook.com>
2023-11-19 18:30:13 +0300

    prettier added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,27 @@
-
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-  <div className={"mainContainer"}>
-    <div className={"titleContainer"}>
+    <div className={'mainContainer'}>
+      <div className={'titleContainer'}>
         <div>Login</div>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input placeholder='Enter your email here' className={'inputBox'} />
+        <label className='errorLabel'></label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input placeholder='Enter your password here' className={'inputBox'} />
+        <label className='errorLabel'></label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input className={'inputButton'} type='button' value={'Log in'} />
+      </div>
     </div>
-    <br />
-    <div className={"inputContainer"}>
-        <input
-            placeholder="Enter your email here"
-            className={"inputBox"} />
-        <label className="errorLabel"></label>
-    </div>
-    <br />
-    <div className={"inputContainer"}>
-        <input 
-            placeholder="Enter your password here"
-            className={"inputBox"} />
-        <label className="errorLabel"></label>
-    </div>
-    <br />
-    <div className={"inputContainer"}>
-        <input
-            className={"inputButton"}
-            type="button"
-    
-            value={"Log in"} />
-    </div>
-</div>
-  );
+  )
 }
 
-export default App;
+export default App
