4d353298af5badd9eb9feaafbdb1e28b9a51d569
ScorpN <sf_commander@outlook.com>
2023-11-20 21:20:07 +0300

    Login panels splited up. Credentiels recevied from inputs.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
-import Medicine from './Services'
+import PhysicianLogin from './Component/PhysicianLogin'
+import PatientLogin from './Component/PatientLogin'
+
 import './App.css'
-import { useEffect } from 'react'
 
 function App() {
-  useEffect(() => {
-    Medicine.getUser()
-  })
-
   return (
     <>
-      <h1>Login</h1>
-      <div>
-        <input placeholder='Enter your TC here' />
-      </div>
-      <div>
-        <input placeholder='Enter your password here' />
-      </div>
-      <div>
-        <input type='button' value={'Log in'} onClick={() => {
-          Medicine.login('30328905022','1234')
-        }} />
-      </div>
+      <PhysicianLogin />
+      <PatientLogin />
     </>
   )
 }
